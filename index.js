c5834ac4f3302d8e73e657704f48a123ea7a0145
DialloVerse <durali@ymail.com>
2022-07-08 17:22:28 +0200

    Completed JS Errors

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const resetBtn = document.querySelector("#resetBtn");
 const gameWidth = gameBoard.width;
 const gameHeight = gameBoard.height;
 const boardBackground = "forestgreen";
-
 const paddle1Color = "lightblue";
 const paddle2Color = "red";
 const paddleBorder = "black";
@@ -14,7 +13,7 @@ const ballBorderColor = "black";
 const ballRadius = 12.5;
 const paddleSpeed = 50;
 let intervalID;
-let ballSpeed = 1;
+let ballSpeed;
 let ballX = gameWidth / 2;
 let ballY = gameHeight / 2;
 let ballXDirection = 0;
@@ -27,7 +26,6 @@ let paddle1 = {
   x: 0,
   y: 0,
 };
-
 let paddle2 = {
   width: 25,
   height: 100,
@@ -58,7 +56,7 @@ function clearBoard() {
   ctx.fillStyle = boardBackground;
   ctx.fillRect(0, 0, gameWidth, gameHeight);
 }
-function drawPaddles(ballX, ballY) {
+function drawPaddles() {
   ctx.strokeStyle = paddleBorder;
 
   ctx.fillStyle = paddle1Color;
@@ -69,10 +67,123 @@ function drawPaddles(ballX, ballY) {
   ctx.fillRect(paddle2.x, paddle2.y, paddle2.width, paddle2.height);
   ctx.strokeRect(paddle2.x, paddle2.y, paddle2.width, paddle2.height);
 }
-function createBall() {}
-function moveBall() {}
-function drawBall() {}
-function checkCollision() {}
-function changeDirection() {}
-function updateScore() {}
-function resetGame() {}
+function createBall() {
+  ballSpeed = 1;
+  if (Math.round(Math.random()) == 1) {
+    ballXDirection = 1;
+  } else {
+    ballXDirection = -1;
+  }
+  if (Math.round(Math.random()) == 1) {
+    ballYDirection = Math.random() * 1; //more random directions
+  } else {
+    ballYDirection = Math.random() * -1; //more random directions
+  }
+  ballX = gameWidth / 2;
+  ballY = gameHeight / 2;
+  drawBall(ballX, ballY);
+}
+function moveBall() {
+  ballX += ballSpeed * ballXDirection;
+  ballY += ballSpeed * ballYDirection;
+}
+function drawBall(ballX, ballY) {
+  ctx.fillStyle = ballColor;
+  ctx.strokeStyle = ballBorderColor;
+  ctx.lineWidth = 2;
+  ctx.beginPath();
+  ctx.arc(ballX, ballY, ballRadius, 0, 2 * Math.PI);
+  ctx.stroke();
+  ctx.fill();
+}
+function checkCollision() {
+  if (ballY <= 0 + ballRadius) {
+    ballYDirection *= -1;
+  }
+  if (ballY >= gameHeight - ballRadius) {
+    ballYDirection *= -1;
+  }
+  if (ballX <= 0) {
+    player2Score += 1;
+    updateScore();
+    createBall();
+    return;
+  }
+  if (ballX >= gameWidth) {
+    player1Score += 1;
+    updateScore();
+    createBall();
+    return;
+  }
+  if (ballX <= paddle1.x + paddle1.width + ballRadius) {
+    if (ballY > paddle1.y && ballY < paddle1.y + paddle1.height) {
+      ballX = paddle1.x + paddle1.width + ballRadius; // if ball gets stuck
+      ballXDirection *= -1;
+      ballSpeed += 1;
+    }
+  }
+  if (ballX >= paddle2.x - ballRadius) {
+    if (ballY > paddle2.y && ballY < paddle2.y + paddle2.height) {
+      ballX = paddle2.x - ballRadius; // if ball gets stuck
+      ballXDirection *= -1;
+      ballSpeed += 1;
+    }
+  }
+}
+function changeDirection(event) {
+  const keyPressed = event.keyCode;
+  const paddle1Up = 87;
+  const paddle1Down = 83;
+  const paddle2Up = 38;
+  const paddle2Down = 40;
+
+  switch (keyPressed) {
+    case paddle1Up:
+      if (paddle1.y > 0) {
+        paddle1.y -= paddleSpeed;
+      }
+      break;
+    case paddle1Down:
+      if (paddle1.y < gameHeight - paddle1.height) {
+        paddle1.y += paddleSpeed;
+      }
+      break;
+    case paddle2Up:
+      if (paddle2.y > 0) {
+        paddle2.y -= paddleSpeed;
+      }
+      break;
+    case paddle2Down:
+      if (paddle2.y < gameHeight - paddle2.height) {
+        paddle2.y += paddleSpeed;
+      }
+      break;
+  }
+}
+function updateScore() {
+  scoreText.textContent = `${player1Score} : ${player2Score}`;
+}
+function resetGame() {
+  player1Score = 0;
+  player2Score = 0;
+  paddle1 = {
+    width: 25,
+    height: 100,
+    x: 0,
+    y: 0,
+  };
+  paddle2 = {
+    width: 25,
+    height: 100,
+    x: gameWidth - 25,
+    y: gameHeight - 100,
+  };
+  ballSpeed = 1;
+  ballX = 0;
+  ballY = 0;
+  ballXDirection = 0;
+  ballYDirection = 0;
+  updateScore();
+  clearInterval(intervalID);
+  gameStart();
+}
